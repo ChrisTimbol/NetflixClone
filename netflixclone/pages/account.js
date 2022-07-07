b6dfba9e3b883927d93ab67b14a9bf08617c1102
Chris Timbol <christimbol707@gmail.com>
2022-07-07 15:12:27 -0700

    position text more left

--- netflixclone/pages/account.js
+++ netflixclone/pages/account.js
@@ -18,7 +18,7 @@ export default function Account() {
                     <div className="text-white relative flex items-center justify-center w-full  h-[400px] border-b-8 border-stone-600" >
 
                         <Image className="z-0 " layout='fill' alt="Image" quality={100} objectFit="cover" priority={true} src="/LandingPage/landingBackground.jpg" />
-                        <div className="absolute top-[20%] p-4 md:p-8">
+                        <div className="absolute top-[40%] left-[10%]  p-4 md:p-8">
                             <h1 className="text-3xl md:text-5xl font-bold"> My Shows</h1>
                         </div>
                     </div>
