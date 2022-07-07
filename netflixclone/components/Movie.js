f426c59bb524b5d27822304527f4632e5bbf29f6
Chris Timbol <christimbol707@gmail.com>
2022-07-07 15:02:11 -0700

    fix row image link, remove scrollbar

--- netflixclone/components/Movie.js
+++ netflixclone/components/Movie.js
@@ -31,7 +31,7 @@ function Movie(props) {
             <div key={props.i} className=" z-0 hover:z-10 hover:scale-125 w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative px-1 ">
 
 
-                <Image layout='responsive' width={240} height={160} quality={100} alt={props.e.title} priority={true} src={`https://image.tmdb.org/t/p/w500/${props.e?.img}`} />
+                <Image layout='responsive' width={240} height={160} quality={100} alt={props.e.title} priority={true} src={`https://image.tmdb.org/t/p/w500/${props.e?.backdrop_path}`} />
                 <div className='flex justify-center items-center absolute bottom-0  top-0 left-0 right-0 hover:bg-black/80 hover:opacity-80 opacity-0 text-white'>
                     <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center text-center  h-full w-full ">
                         {props.e?.title}
