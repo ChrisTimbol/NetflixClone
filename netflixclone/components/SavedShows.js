0e6038d86e8dc0aeb055040ab5a2382e34555c56
Chris Timbol <christimbol707@gmail.com>
2022-07-07 14:55:30 -0700

    vercel deployment bug fixes

--- netflixclone/components/SavedShows.js
+++ netflixclone/components/SavedShows.js
@@ -36,9 +36,10 @@ const SavedShows = (props) => {
             console.log(error)
         }
     }
+    
     return (
         <>
-            <h2 className="text-white font-bold md:text-xl p-4">My Saved Shows</h2>
+            <h2 className="text-white font-bold md:text-xl p-4"> My Saved Shows</h2>
             <div className="relative flex items-center group">
                 <MdChevronLeft
                     onClick={slideLeft}
@@ -51,19 +52,36 @@ const SavedShows = (props) => {
                     id={'slider' + props.rowID}
                 >
                     {movies.map((e, i) => (
-                        <div key={i} className=" z-0 hover:z-10 hover:scale-125 w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative px-1 ">
-                            <Image layout='responsive' width={240} height={160} quality={100} alt={e.title} priority={true} src={`https://image.tmdb.org/t/p/w500/${e?.img}`} />
+                        <div key={i} className="z-0 hover:z-10 hover:scale-125 w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative px-1 ">
+                            <Image 
+                                layout='responsive' 
+                                width={240} height={160} 
+                                quality={100} 
+                                alt={e?.title} 
+                                priority={true} 
+                                src={`https://image.tmdb.org/t/p/w500/${e?.img}`} />
+
                             <div className='flex justify-center items-center absolute bottom-0  top-0 left-0 right-0 hover:bg-black/80 hover:opacity-80 opacity-0 text-white'>
+
                                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center text-center  h-full w-full ">
                                     {e?.title}
                                 </p>
-                                <p onClick={() => { deleteShow(e?.id) }} className="absolute text-gray-300 top-4 right-4"><AiOutlineClose /></p>
+                                <p 
+                                onClick={() => { deleteShow(e?.id) }} 
+                                    className="absolute text-gray-300 top-4 right-4">
+                                    <AiOutlineClose />
+                                </p>
+
                             </div>
                         </div>
                     ))}
                 </div>
 
-                <MdChevronRight onClick={slideRight} className="z-50 bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100  hidden cursor-pointer z-10 group-hover:block" size={40} />
+                <MdChevronRight 
+                    onClick={slideRight} 
+                    className="z-50 bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100  hidden cursor-pointer z-10 group-hover:block" 
+                    size={40} 
+                />
 
             </div>
         </>
