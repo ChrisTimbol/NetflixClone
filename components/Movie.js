ee7d6317c437d4b36f45a224085202b27473e78c
Chris Timbol <christimbol707@gmail.com>
2023-02-27 16:12:29 -0800

    made icon size of like button bigger for users

--- components/Movie.js
+++ components/Movie.js
@@ -37,7 +37,7 @@ export default function Movie(props) {
                 <Image layout='responsive' width={240} height={160} quality={100} alt={props.e?.title} priority={true} src={`https://image.tmdb.org/t/p/w500/${props.e?.backdrop_path}`} />
                
                {/*hover background */}
-                <div className='flex justify-center items-center absolute bottom-0  top-0 left-0 right-0 hover:bg-black/80 hover:opacity-80 opacity-0 text-white'>
+                <div className='flex justify-center items-center absolute bottom-0 mx-1  top-0 left-0 right-0 hover:bg-black/80 hover:opacity-80 opacity-0 text-white'>
                     
                     {/* Displays title in center of movie */}
                     <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center text-center  h-full w-full ">
@@ -45,7 +45,7 @@ export default function Movie(props) {
                     </p>
 
                     <p onClick={saveShow}> {/* If user clicks heart save to DB*/}
-                      {like ? <FaHeart className='absolute top-6 left-5 text-red-500 ' /> :<FaRegHeart className='absolute top-6 w-3 left-5 text-gray-300' />}
+                      {like ? <FaHeart  className='absolute top-2 left-5 text-red-500 text-5xl'/> :<FaRegHeart  className='absolute top-2 left-5 text-gray-300 text-5xl' />}
                     </p>
                 </div>
             </div>
--- components/SavedShows.js
+++ components/SavedShows.js
@@ -35,7 +35,7 @@ const SavedShows = (props) => {
     const deleteShow = async (passedID) => {
         try {
             const result = movies.filter((e) => e.id !== passedID) // filters out show from movies array/state
-           
+
             await updateDoc(movieRef, { // updates the movieRef field in fb
                 savedShows: result,
             })
@@ -43,7 +43,7 @@ const SavedShows = (props) => {
             console.log(error)
         }
     }
-    
+
     return (
         <>
             <h2 className="text-white font-bold md:text-xl p-4"> My Saved Shows</h2>
@@ -57,34 +57,35 @@ const SavedShows = (props) => {
                 <div className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth no-scrollbar scrollbar-hide relative " id={'slider' + props.rowID}>
                     {movies.map((e, i) => (
                         <div key={i} className="z-0 hover:z-10 hover:scale-125 w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative px-1 ">
-                            <Image 
-                                layout='responsive' 
-                                width={240} height={160} 
-                                quality={100} 
-                                alt={e?.title} 
-                                priority={true} 
+                            <Image
+                                layout='responsive'
+                                width={240} height={160}
+                                quality={100}
+                                alt={e?.title}
+                                priority={true}
                                 src={`https://image.tmdb.org/t/p/w500/${e?.img}`} />
 
-                            <div className='flex justify-center items-center absolute bottom-0  top-0 left-0 right-0 hover:bg-black/80 hover:opacity-80 opacity-0 text-white'>
+                            <div className='flex justify-center items-center absolute bottom-0  top-0 left-0 right-0 hover:bg-black/75 m-1 hover:scale-100 hover:opacity-80 opacity-0 text-white'>
 
                                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center text-center  h-full w-full ">
                                     {e?.title}
                                 </p>
-                                <p 
-                                onClick={() => { deleteShow(e?.id) }} 
-                                    className="absolute text-gray-300 top-4 right-4">
-                                    <AiOutlineClose />
-                                </p>
+                            <div className="">
+                            <AiOutlineClose onClick={() => { deleteShow(e?.id) }}
+                                    className="absolute text-2xl text-white-300 top-6 right-4 opacity-100"  />
+                            </div>
+      
+                        
 
                             </div>
                         </div>
                     ))}
                 </div>
 
-                <MdChevronRight 
-                    onClick={slideRight} 
-                    className="z-50 bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100  hidden cursor-pointer z-10 group-hover:block" 
-                    size={40} 
+                <MdChevronRight
+                    onClick={slideRight}
+                    className="z-50 bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100  hidden cursor-pointer z-10 group-hover:block"
+                    size={40}
                 />
 
             </div>
